879ee014a097fe4526757e982378571d8e2708fa
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-27 18:23:18 +0000

    displaying colors

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,5 +1,14 @@
-function ColorList() {
-  return <div>ColorList</div>;
+import { nanoid } from "nanoid";
+import SingleColor from "./SingleColor";
+
+function ColorList({ colors }) {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid} color={color} index={index} />;
+      })}
+    </section>
+  );
 }
 
 export default ColorList;
